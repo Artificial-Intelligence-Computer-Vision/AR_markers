8ac51e7e9c11bc16ea18ea21b94ab769804714d7
RonaldsonBellande <ronaldsonbellande@gmail.com>
2021-08-15 00:30:59 -0400

    for now

---
+++ ar_marker.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+
+
+class ar_marker(object):
+    def __init__(self):
+        
+        # Path to the image
+        self.path = "ar_testing"
+
+
+
+    def feature_detection(self):
+        
+        image =
+
+
+
+    def feature_match(self):
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+
