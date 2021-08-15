e7838b6b4eb759b2865e4e014c66537516049566
RonaldsonBellande <ronaldsonbellande@gmail.com>
2021-08-15 01:51:07 -0400

    for now

--- ar_marker.py
+++ ar_marker.py
@@ -6,17 +6,43 @@ class ar_marker(object):
     def __init__(self):
         
         # Path to the image
-        self.path = "ar_testing"
+        self.path = "ar_testing/"
+
+        # Path to save
+        self.save_path = "results/"
+
+        # Create a sift feature extractor
+        self.featured = cv2.xfeatures2d.SIFT_create()
+
+        self.feature_detection()
+        self.feature_match()
 
 
 
     def feature_detection(self):
         
-        image =
+        # Reading the image
+        image = cv2.imread(self.path + "set_1/book_cover_1.jpg")
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        # Detect features from the image
+        keypoints, descriptors = featured.detectAndCompute(image, None)
+
+        # Draw the detected key points
+        featured_image = cv2.drawKeypoints(gray, keypoints, image)
+        
+        # Save image
+        cv2.imwrite(self.save_path + "book_cover_with_keypoints.jpg", featured_image)
 
 
 
     def feature_match(self):
+        
+        # Reading the images
+        image1 = cv2.imread(self.path + "set_1/book_cover_1.jpg")
+        image2 = cvs.imread(self.path + "set_2/book_cover_2.jpg")
+
+
 
 
 
@@ -25,5 +51,11 @@ class ar_marker(object):
 
 
 if __name__ == "__main__":
-    pass
+   
+
+    # ar_markers make object
+    ar_marker_obj = ar_marker()
+
+
+
 
